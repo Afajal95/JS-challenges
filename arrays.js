167684ed9552c536cd5411ce9604a034b70e3210
Afajal <afzalmemon95@gmail.com>
2021-03-09 21:14:49 +0530

    arrays js challenge solution hacker rank

---
+++ arrays.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    var sorted_array = nums.sort(function(a,b){
+        return a-b;
+    });
+     var array2 = sorted_array.filter(function(elem, index, self){
+         return index=== self.indexOf(elem);
+              })
+              return array2[array2.length - 2];
+}
+
